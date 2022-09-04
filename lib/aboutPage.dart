a4f4f70343d70d5984f63587a3b31d0e1a9358a8
ANKIT <ankitpaul894@gmail.com>
2022-09-04 21:22:29 +0530

    syling done

---
+++ lib/aboutPage.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://www.linkedin.com/in/ankit-paul-914936234/');
+final Uri _url2 = Uri.parse('https://www.linkedin.com/in/arpan-de-001ab31b9/');
+final Uri _url3 =
+    Uri.parse('https://github.com/Ankit-AP-Paul/simple_weather_app');
+
+class AppInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        title: Text('About'),
+        backgroundColor: Color.fromARGB(255, 20, 79, 241),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+          ),
+          Image.asset('images/app_icon.png', height: 150),
+          Text(
+            'Simple Weather App',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Color.fromARGB(255, 20, 79, 241)),
+          ),
+          Text(
+            'v1.0.0',
+            style: TextStyle(
+                fontSize: 20, color: Color.fromARGB(255, 20, 79, 241)),
+          ),
+          Container(
+            height: 40,
+          ),
+          Text(
+            'This is an open source project and can be found on',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          FlatButton(
+            onPressed: _launchUrl3,
+            child: Text(
+              'GitHub',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          Text(
+            'If you liked our work',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          Text(
+            'show some ❤ and ⭐ the repository',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          Container(
+            height: 40,
+          ),
+          Text(
+            'Made with ❤ by',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 20, 79, 241),
+              fontSize: 15,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+            child: FlatButton(
+              onPressed: _launchUrl,
+              child: Text(
+                'Ankit Paul',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+            child: FlatButton(
+              onPressed: _launchUrl2,
+              child: Text(
+                'Arpan De',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
+  }
+}
+
+Future<void> _launchUrl2() async {
+  if (!await launchUrl(_url2)) {
+    throw 'Could not launch $_url2';
+  }
+}
+
+Future<void> _launchUrl3() async {
+  if (!await launchUrl(_url3)) {
+    throw 'Could not launch $_url3';
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:simple_weather_app/data_service.dart';
+import 'package:simple_weather_app/drawerMenu.dart';
 import 'package:simple_weather_app/models.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,102 +19,131 @@ class _MyAppState extends State<MyApp> {
 
   WeatherResponse _response;
 
+  DateTime timeBackPressed = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.lightBlueAccent,
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 50),
-                child: SizedBox(
-                  width: 150,
-                  child: TextField(
-                    controller: _cityTextController,
-                    decoration: InputDecoration(labelText: 'City'),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              RaisedButton(
-                color: Colors.blue.shade800,
-                onPressed: _search,
-                child: Text(
-                  'Search',
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
-          ),
-          if (_response != null)
-            Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.network(_response.iconUrl),
-                      Text(
-                        '${_response.weatherInfo.description} \n in ${_response.cityName}',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.thermostat,
-                      color: Colors.blue,
-                      size: 50,
-                    ),
-                    title: Text(
-                      'Temperature = ${_response.tempInfo.temperature}°C \n Feels Like = ${_response.tempInfo.feelsTemp}°C',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.water_drop,
-                      color: Colors.blue,
-                      size: 50,
-                    ),
-                    title: Text(
-                      'Humidity = ${_response.tempInfo.humid}%',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.air_outlined,
-                      color: Colors.blue,
-                      size: 50,
-                    ),
-                    title: Text(
-                      'Wind Speed = ${(_response.windInfo.wspeed * 3.6).toStringAsFixed(2)} km/h',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-              ],
+        debugShowCheckedModeBanner: false,
+        home: WillPopScope(
+          onWillPop: () async {
+            final difference = DateTime.now().difference(timeBackPressed);
+            final isExitWarning = difference >= Duration(seconds: 2);
+            timeBackPressed = DateTime.now();
+            if (isExitWarning) {
+              final msgg = 'Press Back Again to exit';
+              Fluttertoast.showToast(msg: msgg, fontSize: 18);
+              return false;
+            } else {
+              Fluttertoast.cancel();
+              return true;
+            }
+          },
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 20, 79, 241),
+              title: Center(child: Text('Simple Weather App')),
             ),
-        ]),
-      ),
-    ));
+            drawer: DrawerMenu(),
+            backgroundColor: Colors.lightBlueAccent,
+            body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 50),
+                          child: SizedBox(
+                            width: 150,
+                            child: TextField(
+                              controller: _cityTextController,
+                              decoration: InputDecoration(labelText: 'City'),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                        RaisedButton(
+                          color: Color.fromARGB(255, 20, 79, 241),
+                          onPressed: _search,
+                          child: Text(
+                            'Search',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                    if (_response != null)
+                      Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            color: Colors.blueAccent,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.network(_response.iconUrl),
+                                Text(
+                                  '${_response.weatherInfo.description} \n in ${_response.cityName}',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Card(
+                            color: Colors.blue.shade200,
+                            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.thermostat,
+                                color: Color.fromARGB(255, 20, 79, 241),
+                                size: 50,
+                              ),
+                              title: Text(
+                                'Temperature = ${_response.tempInfo.temperature}°C \n Feels Like = ${_response.tempInfo.feelsTemp}°C',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            color: Colors.blue.shade200,
+                            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.water_drop,
+                                color: Color.fromARGB(255, 20, 79, 241),
+                                size: 50,
+                              ),
+                              title: Text(
+                                'Humidity = ${_response.tempInfo.humid}%',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            color: Colors.blue.shade200,
+                            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.air_outlined,
+                                color: Color.fromARGB(255, 20, 79, 241),
+                                size: 50,
+                              ),
+                              title: Text(
+                                'Wind Speed = ${(_response.windInfo.wspeed * 3.6).toStringAsFixed(2)} km/h',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                  ]),
+            ),
+          ),
+        ));
   }
 
   void _search() async {
